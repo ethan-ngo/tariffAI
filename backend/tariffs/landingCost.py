c4ee42c61f1fb3010b5007c86ad5e9b9a9f080cc
Khiet.L.Huynh (tech.uscadb.Newegg) <kh1d@newegg.com>
2025-07-09 15:34:45 -0700

    fix landing cost calculation

--- backend/tariffs/landingCost.py
+++ backend/tariffs/landingCost.py
@@ -8,10 +8,13 @@ def getLanding(prod_value: float, quantity: int, shipping: float, insurance: int
     subtotal = prod_value * quantity + shipping + insurance
     print("subtotal", subtotal)
 
-    dutyTotal = (tax301 + MRN) * subtotal
+    print("tax301:", type(tax301))
+    print("MRN:", type(MRN))
+
+    dutyTotal = ((tax301 / 100) + (MRN / 100)) * subtotal
     print("dutytotal: ", dutyTotal)
           
-    vatTotal = VAT * dutyTotal
+    vatTotal = (VAT / 100) * dutyTotal
     print("vatTotal ", vatTotal)
     return subtotal + dutyTotal + vatTotal
 
